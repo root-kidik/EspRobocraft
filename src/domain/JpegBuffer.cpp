7f1c019840962c75c50af2530546c61358a2215d
root-kidik <rtkid.nik@gmail.com>
2025-04-10 07:50:55 +0300

    feat: update

--- src/domain/JpegBuffer.cpp
+++ src/domain/JpegBuffer.cpp
@@ -1,5 +1,16 @@
 #include <domain/JpegBuffer.hpp>
 
+#include <iostream>
+
+namespace
+{
+constexpr std::uint8_t kJpegBeginValue      = 0xFF;
+constexpr std::uint8_t kJpegBeginAfterValue = 0xD8;
+
+constexpr std::uint8_t kJpegPreEndValue = 0xFF;
+constexpr std::uint8_t kJpegEndValue    = 0xD9;
+} // namespace
+
 namespace esp_robocraft
 {
 
@@ -9,29 +20,49 @@ JpegBuffer::JpegBuffer(JpegConsumer& consumer) : _consumer{consumer}
 
 void JpegBuffer::add(const std::array<std::uint8_t, 1024>& data, std::size_t size)
 {
+    std::cerr << "read bytes: " << size << '\n';
+    std::cerr << "curr sz: " << _buffer.size() << '\n';
+
     if (size == 0)
         return;
 
     bool is_found = false;
 
     for (std::size_t i = 0; i < size - 1; i++)
-        if (data[i] == 0xFF && data[i + 1] == 0xD9)
+    {
+        if (data[i] == kJpegBeginValue && data[i + 1] == kJpegBeginAfterValue && !_buffer.empty())
         {
-            _buffer.insert(_buffer.end(), data.begin(), data.begin() + i + 2);
-
+            std::cerr << "found: " << i << '\n';
+            _buffer.insert(_buffer.end(), data.begin(), data.begin() + i);
             _consumer.on_jpeg(_buffer);
-
             _buffer.clear();
-
-            _buffer.insert(_buffer.end(), data.begin() + i + 2, data.begin() + size);
-       
+            _buffer.insert(_buffer.end(), data.begin() + i, data.begin() + size);
             is_found = true;
-       
             break;
         }
 
+        // if (data[i] == kJpegPreEndValue && data[i + 1] == kJpegEndValue)
+        // {
+        //     std::cerr << "found: " << i + 2 << '\n';
+
+        //     _buffer.insert(_buffer.end(), data.begin(), data.begin() + i + 2);
+
+        //     std::cerr << "on_jpeg: " << _buffer.size() << '\n';
+        //     _consumer.on_jpeg(_buffer);
+
+        //     _buffer.clear();
+
+        //     _buffer.insert(_buffer.end(), data.begin() + i + 2, data.begin() + size);
+
+        //     is_found = true;
+
+        //     break;
+        // }
+    }
+
     if (!is_found)
     {
+        std::cerr << "not found: " << size << '\n';
         _buffer.insert(_buffer.end(), data.begin(), data.begin() + size);
     }
 }
--- tests/unit/JpegBufferTest.cpp
+++ tests/unit/JpegBufferTest.cpp
@@ -6,11 +6,11 @@
 
 #include <domain/JpegBuffer.hpp>
 
-template<typename... Arrays>
-std::vector<std::uint8_t> vector_from_arrays(const Arrays&... arrays)
+template <typename... Arrays>
+std::vector<std::uint8_t> vector_from_arrays(std::size_t size = 1024, const Arrays&... arrays)
 {
     std::vector<std::uint8_t> vector;
-    (vector.insert(vector.end(), arrays.begin(), arrays.end()), ...);
+    (vector.insert(vector.end(), arrays.begin(), arrays.begin() + +size), ...);
     return vector;
 }
 
@@ -38,7 +38,7 @@ TEST(JpegBuffer, ReadOne)
 
     const auto received_buffer = generate_jpeg();
 
-    EXPECT_CALL(jpeg_consumer, on_jpeg(vector_from_arrays(received_buffer))).WillOnce(testing::Return());
+    EXPECT_CALL(jpeg_consumer, on_jpeg(vector_from_arrays(received_buffer.size(), received_buffer))).WillOnce(testing::Return());
 
     jpeg_buffer.add(received_buffer, received_buffer.size());
 }
@@ -50,13 +50,68 @@ TEST(JpegBuffer, ReadTwo)
     esp_robocraft::JpegBuffer jpeg_buffer{jpeg_consumer};
 
     const std::size_t received_buffer_begin_size = 512;
-    const std::size_t received_buffer_end_size = 512; 
+    const std::size_t received_buffer_end_size   = 512;
 
     const auto received_buffer_begin = generate_jpeg(received_buffer_begin_size, false);
-    const auto received_buffer_end = generate_jpeg(received_buffer_end_size,true);
+    const auto received_buffer_end   = generate_jpeg(received_buffer_end_size, true);
 
-    EXPECT_CALL(jpeg_consumer, on_jpeg(vector_from_arrays(received_buffer_begin, received_buffer_end))).WillOnce(testing::Return());
+    EXPECT_CALL(jpeg_consumer,
+                on_jpeg(vector_from_arrays(received_buffer_begin_size, received_buffer_begin, received_buffer_end)))
+        .WillOnce(testing::Return());
 
     jpeg_buffer.add(received_buffer_begin, received_buffer_begin_size);
-    jpeg_buffer.add(received_buffer_begin, received_buffer_end_size);
+    jpeg_buffer.add(received_buffer_end, received_buffer_end_size);
+}
+
+TEST(JpegBuffer, ReadThree)
+{
+    JpegConsumerMock jpeg_consumer;
+
+    esp_robocraft::JpegBuffer jpeg_buffer{jpeg_consumer};
+
+    const std::size_t sz = 512;
+
+    const auto received_buffer_first_end    = generate_jpeg(sz, true);
+    const auto received_buffer_second_begin = generate_jpeg(sz, false);
+
+    std::array<std::uint8_t, 1024> first;
+    for (std::size_t i = 0; i < sz; i++)
+        first[i] = received_buffer_first_end[i];
+    for (std::size_t i = sz, j = 0; i < sz * 2; i++, j++)
+        first[i] = received_buffer_second_begin[j];
+
+    EXPECT_CALL(jpeg_consumer, on_jpeg(vector_from_arrays(sz, received_buffer_first_end))).WillOnce(testing::Return());
+
+    jpeg_buffer.add(first, sz * 2);
+
+    const auto received_buffer_second_end = generate_jpeg(sz, true);
+
+    EXPECT_CALL(jpeg_consumer, on_jpeg(vector_from_arrays(sz, received_buffer_second_begin, received_buffer_second_end))).WillOnce(testing::Return());
+
+    jpeg_buffer.add(received_buffer_second_end, sz);
+}
+
+TEST(JpegBuffer, ReadN)
+{
+    JpegConsumerMock jpeg_consumer;
+
+    esp_robocraft::JpegBuffer jpeg_buffer{jpeg_consumer};
+
+    const std::size_t received_buffer_begin_size = 512;
+    const std::size_t received_buffer_end_size   = 512;
+
+    const std::size_t count_frames = 10;
+
+    for (std::size_t i = 0; i < count_frames; i++)
+    {
+        const auto received_buffer_begin = generate_jpeg(received_buffer_begin_size, false);
+        const auto received_buffer_end   = generate_jpeg(received_buffer_end_size, true);
+
+        EXPECT_CALL(jpeg_consumer,
+                    on_jpeg(vector_from_arrays(received_buffer_begin_size, received_buffer_begin, received_buffer_end)))
+            .WillOnce(testing::Return());
+
+        jpeg_buffer.add(received_buffer_begin, received_buffer_begin_size);
+        jpeg_buffer.add(received_buffer_end, received_buffer_end_size);
+    }
 }
